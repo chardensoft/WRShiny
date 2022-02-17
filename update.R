6396dc9252aa810b884b2d678973c1912f2fd32a
Chris Harden <68627098+chardensoft@users.noreply.github.com>
2022-02-16 19:33:59 -0800

    Wed Feb 16 19:33:59 PST 2022 Isaac update

--- update.R
+++ update.R
@@ -5,13 +5,14 @@ updateFirebase <- function(MensRunners, WomensRunners, MensTeams, WomensTeams) {
   ############ MEN
   testrunners <- MensRunners
   testrunners <- testrunners[which(testrunners$active == "active"),]
-  if (length(which(colnames(testrunners) %in% c("RUNNER.ID", "uniqueTableID"))) > 0) {
-    testrunners <- testrunners[,-which(colnames(testrunners) %in% c("RUNNER.ID", "uniqueTableID"))]
+  if (length(which(colnames(testrunners) %in% c("uniqueTableID"))) > 0) {
+    testrunners <- testrunners[,-which(colnames(testrunners) %in% c("uniqueTableID"))]
   }
   # testrunners$place <- as.integer(testrunners$place)
   # testrunners$rank <- as.numeric(testrunners$rank)
   # testrunners$previous_rank <- as.numeric(testrunners$previous_rank)
   # testrunners$previous_place <- as.integer(testrunners$previous_place)
+  testrunners$override_rank[length(testrunners$rid)] <- testrunners$rank[length(testrunners$rid)]
   write_json(testrunners, "www/runners.json")
   test_teams <- MensTeams
   write_json(test_teams, "www/teams.json")
@@ -20,13 +21,14 @@ updateFirebase <- function(MensRunners, WomensRunners, MensTeams, WomensTeams) {
   ############ WOMEN
   Wtestrunners <- WomensRunners
   Wtestrunners <- Wtestrunners[which(Wtestrunners$active == "active"),]
-  if (length(which(colnames(Wtestrunners) %in% c("RUNNER.ID", "uniqueTableID"))) > 0) {
-    Wtestrunners <- Wtestrunners[,-which(colnames(Wtestrunners) %in% c("RUNNER.ID", "uniqueTableID"))]
+  if (length(which(colnames(Wtestrunners) %in% c("uniqueTableID"))) > 0) {
+    Wtestrunners <- Wtestrunners[,-which(colnames(Wtestrunners) %in% c("uniqueTableID"))]
   }
   # Wtestrunners$place <- as.integer(Wtestrunners$place)
   # Wtestrunners$rank <- as.numeric(Wtestrunners$rank)
   # Wtestrunners$previous_rank <- as.numeric(Wtestrunners$previous_rank)
   # Wtestrunners$previous_place <- as.integer(Wtestrunners$previous_place)
+  Wtestrunners$override_rank[length(Wtestrunners$rid)] <- Wtestrunners$rank[length(Wtestrunners$rid)]
   write_json(Wtestrunners, "www/Wrunners.json")
   Wtest_teams <- WomensTeams
   write_json(Wtest_teams, "www/Wteams.json")
